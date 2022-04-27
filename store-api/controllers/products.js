c4fcc946cf04b12aabc33eb5abe9e097f74f6cb9
Karthik <karthikajar555@gmail.com>
2022-04-27 22:35:56 +0530

    author updated

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,5 +1,5 @@
 const getAllProducts = (req, res) => {
-  throw new Error("dummy");
+  // throw new Error("dummy");
   res.send({ msg: "all products" });
 };
 const getAllStaticProducts = (req, res) => res.send({ msg: "static products" });
